c4a31ff59e04f8ee61542c35e427ab3cf206a089
Cezário <78759993+CaricaturiJosias@users.noreply.github.com>
2023-10-24 23:25:04 +0000

    Compare corrected

--- Representation/Values/Number.cxx
+++ Representation/Values/Number.cxx
@@ -168,8 +168,24 @@ namespace Value {
     }
 
     bool Number::operator<(Number input) {
-        if (value == 0 && input.getValue() == 0) {
-            return (Mvalue < input.getMvalue());
+         if (value > 0) {
+            return value < input.getValue();
+        } else if (input.getValue() > 0) {
+            return false;
+        } else if (Mvalue > 0) { // Both value and input's value are 0 or negative from here
+            return Mvalue < input.getMvalue();
+        } else if (!(value == 0 && input.getValue() == 0)) { // Both not 0, so 0 > -1 or -1 > -1 or -1 > -4
+            return value < input.getValue();
+        }
+        // normal value is hopeless (0 and 0)
+        if (Mvalue > 0) {
+            return Mvalue < input.getMvalue();
+        } else if (input.getMvalue() > 0) {
+            return false;
+        } else if (Mvalue > 0) { // Both Mvalue and input's Mvalue are 0 or negative from here
+            return Mvalue < input.getMvalue();
+        } else if (!(Mvalue == 0 && input.getMvalue() == 0)) { // Both not 0, so 0 > -1 or -1 > -1 or -1 > -4
+            return Mvalue < input.getMvalue();
         }
         return (value < input.getValue());
     }
